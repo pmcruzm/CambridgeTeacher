e24f7d706cb7ad4a9721e035e7d368831872a00a
David Mateos <damarev@gmail.com>
2016-07-04 14:24:11 +0200

    fix metas redirección mobile (2)

--- js/application.js
+++ js/application.js
@@ -164,7 +164,7 @@ jQuery(document).ready(function(){
 	w_win=jQuery(window).width();
 
 	//Redirección a la página mobile
-	if(w_win<600 && jQuery('meta[property="mobile-redirect"]').attr('content')=="true"){
+	if(device=="yes" && w_win<600 && jQuery('meta[property="mobile-redirect"]').attr('content')=="true"){
 		var url_block=jQuery('meta[property="mobile-redirect-url"]').attr('content');
 		window.top.location = url_block;
 	}
