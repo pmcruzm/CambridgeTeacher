aa45fb58bc9c1a687947af10b868a84a0b11ddfd
David Mateos <damarev@gmail.com>
2020-05-19 12:10:50 +0200

    GA removed

--- js/application.js
+++ js/application.js
@@ -25,16 +25,16 @@ function loadAnalytics() {
 	var facebook_pixel_id=jQuery('meta[property="facebook-pixel-id"]').attr('content');
 
 	//Añadimos GA
-	if( cod_GA && cod_GA != '' ) {
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', cod_GA , 'auto');
-		ga('send', 'pageview');
+	// if( cod_GA && cod_GA != '' ) {
+	// 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	// 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	// 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	// 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	// 	ga('create', cod_GA , 'auto');
+	// 	ga('send', 'pageview');
+	// }
 
-	}
-
+	//GTM
 	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
